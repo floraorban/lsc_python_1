17e7510b0be400e4709b677f898b5b87f7c6a2c2
flora <floraorban@gmail.com>
2023-10-06 11:47:26 +0200

    asset file created

---
+++ story.py
@@ -0,0 +1,17 @@
+person = ''
+adjective = ''
+place = ''
+food = ''
+thing_1 = ''
+thing_2 = ''
+feeling = ''
+celebrity = ''
+song_title = ''
+verb = ''
+
+print("""%s házában voltam bulin, most értem haza.
+Képzeld, mi történt! A pizza, amit rendeltünk, elképesztően %s volt!
+A(z) %s környékén sem ettem még ilyet! Finomabb volt, mint a legutóbbi %s!
+Még a széle is meg volt töltve, volt benne %s és %s! Milyen %s már!
+A tévében közben %s énekelte a %st. Ettől úgy belelkesedtem, hogy muszáj voltam felálni és %s!
+""" % (person, adjective, place, food, thing_1, thing_2, feeling, celebrity, song_title, verb))
